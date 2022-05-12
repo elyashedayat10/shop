534b1980a9e25a04f7493f9e8661e7b5038b457d
elyas <elyashedayat1380@gmail.com>
2022-05-12 05:21:47 +0430

    divide requirements.txt into diffrent madoules

--- products/urls.py
+++ products/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 app_name = "products"
 urlpatterns = [
-    path("list_admin/", ProductsListAdmin, name="list_admin"),
+    path("list_admin/", ProductsListAdmin.as_view(), name="list_admin"),
     path("admin/<slug:slug>/", ProductDetailView.as_view(), name="detail_admin"),
     path("create/", ProductCreateView.as_view(), name="create"),
     path("update/<slug:slug>/", ProductUpdateView.as_view(), name="update"),
